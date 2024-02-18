926511ee250b5ca8b02e71973db63764eb4e46c9
lukinio23 <lukinio4242@gmail.com>
2024-02-18 16:17:09 +0400

    fourth commit

---
+++ day 4/homework/lesson4.py
@@ -0,0 +1,14 @@
+mom_age =39
+dad_age =37
+grandma_age =63
+grandma2_age =82
+grandpa_age =59
+brother_age =4
+sister_age =6
+print("mom wil be " + str(mom_age) + "in 5 years")
+print("dad wil be " + str(dad_age) + "in 5 years")
+print("grandma wil be " + str(grandma_age) + "in 5 years")
+print("grandma2 wil be " + str(grandma2_age) + "in 5 years")
+print("grandpa wil be " + str(grandpa_age) + "in 5 years")
+print("brother wil be " + str(brother_age) + "in 5 years")
+print("sister wil be " + str(sister_age) + "in 5 years")
